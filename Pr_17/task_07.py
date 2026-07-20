1f8056e673a8144a29da67d20e3efb2170b3f3ad
Dmitriy <dmitriy.hellwig@gmail.com>
2026-07-20 11:57:48 +0200

    8

--- Pr_17/task_07.py
+++ Pr_17/task_07.py
@@ -6,9 +6,54 @@ filter_by_min_age(students: list[Student], min_age: int),
 
 """
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
 from pprint import pprint
 
+class Student:
+    MIN_AGE = 16
 
+    student_id_seq = 0
+
+    def __init__(self, name, birth_date):
+        Student.student_id_seq += 1
+
+        self.student_id = Student.student_id_seq
+
+        self.name = name
+        self.birth_date = datetime.strptime(birth_date, "%Y-%m-%d").date()
+
+        if self.get_age() < Student.MIN_AGE:
+            raise ValueError("Student must be at least 16 years old.")
+
+
+    def get_age(self):
+        return relativedelta(datetime.now(), self.birth_date).years
+
+    def calculate_age_on(self, target_date):
+        return relativedelta(target_date, self.birth_date).years
+
+    def __str__(self):
+        return f"Student: {self.name}, birth_date: {self.birth_date}, ID: {self.student_id}"
+
+    def __repr__(self):
+        return f"Student: {self.name}, birth_date: {self.birth_date}, age: {self.get_age()}, ID: {self.student_id}"
+
+    def show_info(self):
+        print(f"Student:")
+        print(f"\tName: {self.name}")
+        print(f"\tAge: {self.get_age()}")
+        print(f"\tID: {self.student_id}")
+
+    @classmethod
+    def from_string(cls, string):
+        name, birth_date = string.split(", ")
+
+        return cls(name, birth_date)
+
+    @classmethod
+    def filter_by_min_age(cls, students, min_age):
+        return [student for student in students if student.get_age() >= min_age]
 
 
 s1 = Student("Alice", "2005-05-10")
